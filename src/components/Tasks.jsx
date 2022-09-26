76146e2267655363b8c8ba61a525c0cdb3c4ace7
Eduardo Coelho <104034703+oducoelho@users.noreply.github.com>
2022-09-26 17:46:02 -0300

    Update Tasks.jsx

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,25 +1,29 @@
 import { EmptyToDo } from './EmptyToDo';
 import { Trash } from 'phosphor-react'
+import { BsFillCheckCircleFill } from 'react-icons/bs'
 import styles from './Tasks.module.css';
 
-export const Tasks = () => {
-  return (
-    <div className={styles.content}>
-      <div className={styles.line}>
-        <div className={styles.component}>
-        <input type="checkbox"/>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, 
-          <Trash />
-        </div>
 
-        <div className={styles.component}>
-        <input type="checkbox"/>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, 
-          <Trash />
-        </div>
-        
-        <EmptyToDo />
-      </div>
+export const Tasks = ({ tasks, onDeleteTask }) => {
+  const handleDeleteTask = () => {
+    onDeleteTask(tasks)
+  }
+
+  return (
+
+    <div className={styles.teste}>
+      <div className={styles.task}>
+      <button className={styles.checkContainer}>
+        <div/>
+      </button>
+      <p>
+        {tasks}
+      </p>
+
+      <button onClick={handleDeleteTask}  className={styles.deleteButton}>
+        <Trash size={20} className={styles.trash}/>
+      </button>
+    </div>
     </div>
   )
 }
